bbbc42a8ffba64f04a00e0dc4adc057b0767e862
sinahamidi <user21.tfa@gmail.com>
2024-12-03 11:18:46 +0330

    remove import of RouterOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { TablesComponent } from './tables/tables.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, TablesComponent],
+  imports: [ FormsModule, TablesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
